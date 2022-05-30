b08744b8d667af635961280d7b82b1f458aeef3f
MnZn <2776835685@qq.com>
2022-05-30 11:00:19 +0800

    pref: use consoleLoop api

--- src/test/java/LexTest.java
+++ src/test/java/LexTest.java
@@ -1,5 +1,6 @@
 import com.mnzn.lex.LexParser;
 import com.mnzn.lex.Token;
+import com.mnzn.utils.sys.SystemUtils;
 import com.mnzn.utils.visual.console.PrintUtils;
 
 import java.util.List;
@@ -12,7 +13,7 @@ public class LexTest {
 
     private static void testFile() {
         LexParser lexParser = new LexParser();
-        String str = "./test_script/test.m";
+        String str = "../resources/test_script/test.m";
         List<Token> tokens = lexParser.parseFile(str);
         PrintUtils.printTokens(tokens);
         PrintUtils.printColorTokens(tokens);
@@ -20,12 +21,10 @@ public class LexTest {
 
     private static void testConsole() {
         LexParser lexParser = new LexParser();
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNext()) {
-            String str = scanner.nextLine();
+        SystemUtils.consoleLoopLine(str -> {
             List<Token> tokens = lexParser.parse(str);
             PrintUtils.printTokens(tokens);
             PrintUtils.printColorTokens(tokens);
-        }
+        });
     }
 }
