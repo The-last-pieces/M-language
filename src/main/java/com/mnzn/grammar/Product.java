58d802ecf0bc6e2340e016e2478c4ae186fd7670
MnZn <2776835685@qq.com>
2022-05-31 17:10:17 +0800

    feature: remove rule field , pref ProduceBuilder api logic

--- src/main/java/com/mnzn/grammar/Product.java
+++ src/main/java/com/mnzn/grammar/Product.java
@@ -1,9 +1,6 @@
 package com.mnzn.grammar;
 
 import com.mnzn.lex.TokenTag;
-import com.mnzn.utils.tree.TreeNode;
-import com.mnzn.utils.tree.TreeUtils;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.io.BufferedReader;
@@ -26,69 +23,26 @@ public class Product {
         }
     }
 
-    @AllArgsConstructor
-    private static class RuleTreeNode implements TreeNode<RuleTreeNode> {
-        int index; // 结点的值,代表第index个symbol
-        List<RuleTreeNode> children; // 子symbol列表
-
-        @Override
-        public List<RuleTreeNode> getChildren() {
-            return children.stream().toList();
-        }
-
-        @Override
-        public void addChild(RuleTreeNode child) {
-            children.add(child);
-        }
-    }
-
     @Getter
     private final String left; // 左部,非终结符的名字
     @Getter
     private final List<Symbol> symbols; // 右部,每个元素都是一个文法符号
-    private final RuleTreeNode rule; // AST的构造规则
 
-    private Product(String left, List<Symbol> symbols, String rule) {
+    private Product(String left, List<Symbol> symbols) {
         this.left = left;
-        this.symbols = symbols;
-        this.rule = initRule(rule);
-    }
-
-    // ASNode的构造规则root[child...]
-    // 例: 0[1,2,3]
-    private RuleTreeNode initRule(String rule) {
-        // 辅助索引去重
-        final Set<Integer> set = new HashSet<>();
-        return TreeUtils.buildTree(rule, str -> {
-            int v = Integer.parseInt(str);
-            if (set.contains(v))
-                throw new IllegalArgumentException(String.format("索引重复: %d", v));
-            if (v > getSymbolCount())
-                throw new IllegalArgumentException(String.format("索引 %d 超出范围: [0,%d)", v, getSymbolCount()));
-            set.add(v);
-            return new RuleTreeNode(v, new ArrayList<>());
-        });
+        this.symbols = symbols.stream().anyMatch(c -> c.isTerminal() && c.terminal == TokenTag.Epsilon) ? new ArrayList<>() : symbols;
     }
 
     // 构造结构和rule相同的ASTNode
     public ASTNode buildASNode(ASTNode[] nodes) {
         if (nodes.length != getSymbolCount())
             throw new IllegalArgumentException(String.format("节点数量不匹配: (提供)%d != (需要)%d", nodes.length, getSymbolCount()));
-        // 通过层序遍历rule的结构进行构造
-        Queue<RuleTreeNode> queue = new LinkedList<>();
-        queue.add(rule); // 根节点
-        while (!queue.isEmpty()) {
-            // 待添加子节点的节点
-            RuleTreeNode parent = queue.poll();
-            for (RuleTreeNode child : parent.children) {
-                // 将子节点也加入队列中
-                queue.add(child);
-                // 链接子节点和父节点
-                nodes[parent.index].addChild(nodes[child.index]);
-            }
-        }
-        // 返回根节点
-        return nodes[rule.index];
+        // 简化语法树
+        if (nodes.length == 1) return nodes[0];
+        // 构造ASTNode
+        ASTNode root = new ASTNode(ProduceTag.of(left));
+        Arrays.stream(nodes).forEach(root::addChild);
+        return root;
     }
 
     // 获取第i个符号
@@ -166,27 +120,34 @@ public class Product {
             return this;
         }
 
-        // 添加一个普通产生式,使用空格分割symbol
-        public ProductBuilder add(String rule, String product) {
+        // 添加一个产生式,使用空格分割symbol , -> 表示and产生式 , |> 表示or产生式
+        public ProductBuilder add(String product) {
+            if (product.contains("->")) {
+                return addAnd(product);
+            } else if (product.contains("|>")) {
+                return addOr(product);
+            } else {
+                throw new IllegalArgumentException("产生式格式错误: string -> string...");
+            }
+        }
+
+        public ProductBuilder addAnd(String product) {
             String[] parts = product.split("->");
-            if (parts.length != 2) throw new IllegalArgumentException("产生式格式错误: string -> string...");
+            if (parts.length != 2) throw new IllegalArgumentException(String.format("产生式格式错误: %s", product));
             String left = parts[0].trim();
             List<Symbol> rights = new ArrayList<>();
-            Arrays.stream(parts[1].trim().split("\\s+")).map(this::node).forEach(rights::add);
-            products.add(new Product(left, rights, rule));
+            Arrays.stream(parts[1].trim().split("\\s+")).
+                    map(this::node).forEach(rights::add);
+            products.add(new Product(left, rights));
             return this;
         }
 
-        // 添加一个别名产生式,使用空格分割symbol
-        // 例: addOr("a->b c") 等价于 add("0","a->b").add("0","a->c")
         public ProductBuilder addOr(String product) {
-            String[] parts = product.split("->");
-            if (parts.length != 2) throw new IllegalArgumentException("产生式格式错误: string -> string...");
+            String[] parts = product.split("\\|>");
+            if (parts.length != 2) throw new IllegalArgumentException(String.format("产生式格式错误: %s", product));
             String left = parts[0].trim();
-            Arrays.stream(parts[1].trim().split("\\s+")).forEach(c -> {
-                // 单点映射,rule取"0"即可
-                products.add(new Product(left, List.of(node(c)), "0"));
-            });
+            Arrays.stream(parts[1].trim().split("\\s+")).
+                    forEach(c -> products.add(new Product(left, List.of(node(c)))));
             return this;
         }
 
@@ -197,15 +158,17 @@ public class Product {
                 String all = String.join("\n", reader.lines().toList());
                 all = all.replaceAll("(?m)(//[\\s\\S]*?(\n|$))", "\n");
                 // 按行分割并去除空行
-                List<String> lines = Arrays.stream(all.split("\n")).
-                        filter(s -> !s.trim().isEmpty()).toList();
-                for (int i = 0, n = lines.size(); i < n; i++) {
-                    String line = lines.get(i);
-                    // 使用add
-                    if (line.matches("\\s*\\S+\\s*->.*")) {
-                        add(lines.get(++i), line);
-                    } else { // 使用addOr
-                        addOr(line.replaceFirst("\\|>", "->"));
+                List<String> lines = Arrays.stream(all.split("\n")).map(String::trim).
+                        filter(s -> !s.isEmpty()).toList();
+                StringBuilder sb = new StringBuilder();
+                for (String line : lines) {
+                    sb.append(' ');
+                    if (line.endsWith("$")) {
+                        sb.append(line, 0, line.length() - 1);
+                        add(sb.toString());
+                        sb.setLength(0);
+                    } else {
+                        sb.append(line);
                     }
                 }
             } catch (IOException e) {
