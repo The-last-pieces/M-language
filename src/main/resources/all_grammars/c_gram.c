3ae469b548dacc47f037e93950033d47703d8af9
MnZn <2776835685@qq.com>
2022-06-08 22:28:48 +0800

    style: remove unuseful comment

--- src/main/resources/all_grammars/c_gram.c
+++ src/main/resources/all_grammars/c_gram.c
@@ -118,6 +118,3 @@ ass_expr -> ass_able ass_op ass_expr $
 ass_expr |> cond_expr $
 ass_op   |> = += -= *= /= %= **= <<= >>= &= ^= |= $
 
-
-// 测试输入
-// /*  Simple prime number generator  */ count = 1; n = 1; limit = 100; while (n < limit) {     k=3;     p=1;     n=n+2;     while ((k*k<=n) && (p)) {         p=n/k*k!=n;         k=k+2;     }     if (p) {         print(n, " is prime ");         count = count + 1;     } } print("Total primes found: ", count, " ");
